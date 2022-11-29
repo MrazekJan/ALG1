082e3937f9c2cf29ee7305daa3b50e70afadbce7
MrazekJan <jan.mrazek.1@seznam.cz>
2022-11-29 13:31:46 +0100

    sync do 22.11

--- ArraysTools/src/ArrayTools.java
+++ ArraysTools/src/ArrayTools.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 public final class ArrayTools {
     private ArrayTools(){
 
@@ -47,7 +50,67 @@ public final class ArrayTools {
             System.out.print(a[i] + ", ");
         }
 
-        System.out.print(a[a.length-1] +"]");
+        System.out.println(a[a.length-1] +"]");
     }
+
+    public static boolean IsAscending(int [] a){
+        int current;
+        for (int i =0; i < a.length;i++){
+            current = a[i];
+            if(i == a.length -1){
+                break;
+            }
+            else if(current > a[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean IsDescending(int [] a){
+        int current;
+        for (int i =0; i < a.length;i++){
+            current = a[i];
+            if(i == a.length -1){
+                break;
+            }
+            else if(current < a[i+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void ReverseArray(int [] a){
+        for(int i = 0; i <= a.length / 2; i++){
+            int firstValue =  a[i];
+            int lastValue =  a[a.length - 1 -i];
+            a[i] = lastValue;
+            a[a.length - 1 -i] = firstValue;
+        }
+
+    }
+
+    public static void ReverseArrayv2(int [] a){
+        for(int i = a.length -1; i >= 0; i--){
+            a[a.length-1-i] = a[i];
+        }
+    }
+
+    public static int[] CreateRandomArray(int length, int min, int max){
+        int [] a = new int[length];
+        for(int i = 0; i < length; i++){
+            Random rand = new Random();
+            a[i] = rand.nextInt(min, max);
+        }
+
+        return a;
+
+    }
+
+
+
 
 }
--- ArraysTools/src/Main.java
+++ ArraysTools/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int [] a = {3,5,2};
+        int [] a = {1,2,3,4};
         System.out.println("Součet: " + ArrayTools.sum(a));
         System.out.println("Součin: " + ArrayTools.product(a));
         System.out.println("Max: " + ArrayTools.max(a));
         System.out.println("Min: " + ArrayTools.min(a));
+        System.out.println("Je vzestupné? " + ArrayTools.IsAscending(a));
+        System.out.println("Je sestupné? " + ArrayTools.IsDescending(a));
         ArrayTools.print(a);
+        ArrayTools.ReverseArray(a);
+        ArrayTools.print(a);
+        ArrayTools.print(ArrayTools.CreateRandomArray(5,1,100));
     }
 }
